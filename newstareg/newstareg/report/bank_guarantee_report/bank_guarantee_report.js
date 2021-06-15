96fabacc334237735e1a34d7385c89f9ae79b1a3
mghoneem10 <mghoneem10@gmail.com>
2021-06-15 19:56:51 +0000

    bank gruntee report

---
+++ newstareg/newstareg/report/bank_guarantee_report/bank_guarantee_report.js
@@ -0,0 +1,26 @@
+// Copyright (c) 2016, erpcloud.systems and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Bank Guarantee Report"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"reqd": 1,
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+		},
+				{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"reqd": 1,
+			"default": frappe.datetime.get_today()
+		},
+
+	]
+};
+
