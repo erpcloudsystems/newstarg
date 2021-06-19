1036ba1625b73b42a591aa5605f4844b23baba95
mghoneem10 <mghoneem10@gmail.com>
2021-06-19 11:47:00 +0000

    bank gurantee add deduction purpose

--- newstareg/newstareg/report/bank_guarantee_report/bank_guarantee_report.js
+++ newstareg/newstareg/report/bank_guarantee_report/bank_guarantee_report.js
@@ -8,19 +8,26 @@ frappe.query_reports["Bank Guarantee Report"] = {
 			"fieldname":"from_date",
 			"label": __("From Date"),
 			"fieldtype": "Date",
-			"width": "80",
-			"reqd": 1,
-			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"width": "80"
 		},
 				{
 			"fieldname":"to_date",
 			"label": __("To Date"),
 			"fieldtype": "Date",
-			"width": "80",
-			"reqd": 1,
-			"default": frappe.datetime.get_today()
+			"width": "80"
 		},
-
+		{
+			"fieldname":"bank_guarantee_purpose",
+			"label": __("Purpose"),
+			"fieldtype": "Select",
+			"options":  ["شيك","شيك ضمان","شيك امانات","شيك تسهيل"],
+		},
+		{
+			"fieldname":"type_of_letter_of_guarantee",
+			"label": __("Type"),
+			"fieldtype": "Select",
+			"options":  ["Cheque","Cash","Bank Guarantee"]
+		}
 	]
 };
 
