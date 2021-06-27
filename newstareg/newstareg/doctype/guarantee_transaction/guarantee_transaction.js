ff862d5210c25d0a7d3dd602f4338f13226a30c9
mghoneem10 <mghoneem10@gmail.com>
2021-06-27 05:56:53 +0000

    gurantee transaction

--- newstareg/newstareg/doctype/guarantee_transaction/guarantee_transaction.js
+++ newstareg/newstareg/doctype/guarantee_transaction/guarantee_transaction.js
@@ -6,3 +6,102 @@ frappe.ui.form.on('Guarantee Transaction', {
 
 	// }
 });
+frappe.ui.form.on("Guarantee Transaction", {
+	setup: function(frm) {
+		frm.set_query("reference_party_type", function() {
+			return {
+				filters: [
+					["DocType", "name", "in", ["Customer","Supplier"]]
+				]
+			};
+		});
+	}
+});
+frappe.ui.form.on("Guarantee Transaction", {
+	setup: function(frm) {
+		frm.set_query("party_type_from", function() {
+			return {
+				filters: [
+					["DocType", "name", "in", ["Customer","Supplier"]]
+				]
+			};
+		});
+	}
+});
+frappe.ui.form.on("Guarantee Transaction", {
+	setup: function(frm) {
+		frm.set_query("party_type_to", function() {
+			return {
+				filters: [
+					["DocType", "name", "in", ["Customer","Supplier"]]
+				]
+			};
+		});
+	}
+});
+frappe.ui.form.on("Guarantee Transaction", {
+	setup: function(frm) {
+		frm.set_query("bank_account_from", function() {
+			return {
+				filters: [
+					["Bank Account","bank", "in", frm.doc.bank_from]
+				]
+			};
+		});
+	}
+});
+frappe.ui.form.on("Guarantee Transaction", {
+	setup: function(frm) {
+		frm.set_query("bank_account_to", function() {
+			return {
+				filters: [
+					["Bank Account","bank", "in", frm.doc.bank_to]
+				]
+			};
+		});
+	}
+});
+frappe.ui.form.on("Guarantee Transaction", {
+	setup: function(frm) {
+		frm.set_query("bank_account_from_acc", function() {
+			return {
+				filters: [
+					["Account","account_type", "in", "Bank"]
+				]
+			};
+		});
+	}
+});
+frappe.ui.form.on("Guarantee Transaction", {
+	setup: function(frm) {
+		frm.set_query("bank_account_to_acc", function() {
+			return {
+				filters: [
+					["Account","account_type", "in", "Bank"]
+				]
+			};
+		});
+	}
+});
+frappe.ui.form.on("Guarantee Transaction", {
+	setup: function(frm) {
+		frm.set_query("party_account_from", function() {
+			return {
+				filters: [
+					["Account","account_type", "in", ["Payable","Receivable"]]
+				]
+			};
+		});
+	}
+});
+frappe.ui.form.on("Guarantee Transaction", {
+	setup: function(frm) {
+		frm.set_query("account_paid_to", function() {
+			return {
+				filters: [
+					["Account","account_type", "in", ["Payable","Receivable"]]
+				]
+			};
+		});
+	}
+});
